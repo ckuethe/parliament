8373b398c29af8da776e906e6527644e308fe0a5
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-02-22 16:40:49 -0800

    add toggle for "-x" flag to neato

--- miniplotter.sh
+++ miniplotter.sh
@@ -3,6 +3,10 @@
 # http://graphviz.org/doc/info/shapes.html#polygon
 # http://graphviz.org/doc/info/attrs.html#d:shape
 
+#un-comment to "prune isolated nodes and peninsulas",
+# thus removing uninteresting structure and clutter
+#XFLAG="-x"
+
 ( \
 	echo "graph {" ; \
 	echo 'graph [size="18,10" fontsize=7 splines=polyline ]' ; \
@@ -10,6 +14,6 @@
 	echo 'node [shape=point size=2]' ; \
 	cat network.txt | sort -u ; \
 	echo "}"; \
-) | time dot -x -Kneato -Tsvg -onetwork.svg
+) | time dot $XFLAG -Kneato -Tsvg -onetwork.svg
 
 convert -density 150 -size 1280x800 network.svg network.png
