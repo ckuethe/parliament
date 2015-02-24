d114ccab6148f5617f9c329d684a21eebe92c0e2
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-02-23 23:48:38 -0800

    interactively build bayes database from json logs

---
+++ bayes_trainer.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import ConfigParser
+import sqlite3
+from parliament_utils import *
+import reverend.thomas
+
+class myTokenizer:
+	def __init__(self):
+		pass
+
+	def tokenize(self, s):
+		stopwords = 'a an am as at and are but can eg not now for i i\'m ie if it is in into to the this my of on our or rt we with you us was when what who how than then they which'.split()
+		s = s.lower().encode('utf-8') # XXX sanitize() should have transcoded this to ascii...
+		s = re.sub('(http|s?ftp|imap|pop3|telnet|ssh)s?:/*\S+', '', s)
+		s = s.translate(None, '<>(){}[]=&!$^|+;:@,."-?')
+
+		# XXX does set() improve or impair classification quality?
+		tokens = sorted(set(s.split()))
+		for w in stopwords:
+			try:
+				tokens.remove(w)
+			except ValueError:
+				pass
+
+		#print "\n========================================================================"
+		#print "\ntokenize(): ", tokens
+		return tokens
+
+def main():
+	dat = 'bayes.dat'
+	tokenizer = myTokenizer()
+	classifier = reverend.thomas.Bayes(tokenizer)
+	try:
+		classifier.load(dat)
+	except IOError:
+		print "INFO: unable to load bayes file '%s'" % dat
+		pass
+
+	n = 0
+	for f in sys.argv[1:] :
+		print "open(%s)" % f
+		fd = open(f, 'r')
+		for js in fd.readlines() :
+			tweet = json.loads(js)
+			if 'text' not in tweet:
+				continue # can't classify non-text tweets
+
+			# "lang_%s" generates a meaningful token
+			# screen_name generates a meaningful token
+			text = sanitize("lang_%s lang_%s %s %s" %
+				(tweet['user']['lang'], tweet['lang'],
+				tweet['user']['screen_name'],
+				tweet['text']) )
+
+
+			labels = classifier.guess(text)
+			if len(labels) == 0:
+				mute = False # default action if the classfier can't answer
+				confidence = 0.5
+			else:
+				if labels[0][0] == 'yes':
+					mute = False
+				else:
+					mute = True
+				confidence = labels[0][1]
+
+			if confidence >= 0.98:
+				# classifier is sure of itself, don't offer to re-train
+				continue
+
+			print ""
+			if mute:
+				print "*plonk*"
+				# continue	# uncomment to not retrain muted items
+			else:
+				print "=(^.^)=      shiny!!"
+
+			tweetparse(tweet)
+			print labels
+			k = kbhit('up-/down-vote?') # returns '+0- xq'
+			print ""
+			if k == '+' :
+				# print "train('yes')"
+				# XXX untrain no?
+				classifier.train('yes', text)	
+			elif k == '-' :
+				# print "train('no')"
+				# XXX untrain yes?
+				classifier.train('no', text)
+			elif k == 'x' :
+				# print "forgetting..."
+				classifier.untrain('yes', text)
+				classifier.untrain('no', text)
+			elif k == 'q' :
+				classifier.save(dat)
+				sys.exit(0)
+			else :
+				# print "no-op"
+				pass
+
+			n += 1
+			if (n % 25) == 0 : # save classifier state
+				classifier.save(dat)
+		fd.close()
+		try:
+			classifier.save(dat)
+		except Exception:
+			print "WARN: unable to load bayes file '%s'" % dat
+			pass
+		print ""
+
+if __name__ == '__main__':
+	main()
+
