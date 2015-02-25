10508332f91cfb2922d82bd78e4fc23a86885f0a
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-02-25 11:31:30 -0800

    a tool to display the json logs

---
+++ jsdump.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+''' this is where i test all my in-line fixups'''
+
+import os
+import sys
+import json
+from parliament_utils import *
+
+def main():
+	for f in sys.argv[1:] :
+		fd = open(f, 'r')
+		for js in fd.readlines() :
+			tweet = json.loads(js)
+			if 'text' not in tweet:
+				continue;
+
+			tweetparse(tweet, '')
+
+		fd.close()
+		print ""
+
+if __name__ == '__main__':
+	main()
+
