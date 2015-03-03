c83892b6919bb633e21513813b4904883e70c122
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-03-03 15:38:11 -0800

    deduping and bayes filtering available

--- jsdump.py
+++ jsdump.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
-''' this is where i test all my in-line fixups'''
 
 import os
 import sys
 import json
 from parliament_utils import *
+import reverend.thomas
+from optparse import OptionParser
 
 def main():
-	for f in sys.argv[1:] :
+	parser = OptionParser()
+	parser.add_option( "-b", "--bayesfile", dest="bayesfile", default=None, help="FILE to operate on.", metavar="FILE")
+	parser.add_option( "-d", "--dedup", dest="dedup", default=False, action="store_true", help="suppress excessively similar tweets")
+	(options, args) = parser.parse_args()
+
+	dedup = None
+	if options.dedup:
+		dedup = {'None': True}
+
+	classifier = None
+	if options.bayesfile:
+		tokenizer = myTokenizer()
+		classifier = reverend.thomas.Bayes(tokenizer)
+		try:
+			classifier.load(options.bayesfile)
+		except IOError:
+			print "INFO: unable to load bayes file '%s'" % options.bayesfile
+			pass
+
+	for f in args :
 		fd = open(f, 'r')
 		for js in fd.readlines() :
-			try:
-				tweet = json.loads(js)
-				if 'text' not in tweet:
-					continue;
+			tweet = json.loads(js)
+			if 'text' not in tweet:
+				continue # can't classify non-text tweets
 
-				tweetparse(tweet, '')
-			except:
-				pass
-
+			tweetparse(tweet, classifier=classifier, dedup=dedup)
 		fd.close()
-		print ""
 
 if __name__ == '__main__':
 	main()
-
