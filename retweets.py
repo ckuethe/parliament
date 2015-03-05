a1ced839f1d5a3a1dd788747779900d7d54d03fe
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-03-04 17:24:13 -0800

    deduplicate tweets and change output format

--- retweets.py
+++ retweets.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 
-import json
 import sys
-import re
-from HTMLParser import HTMLParser
-from unicodedata import normalize
+import json
+from parliament_utils import sanitize
 
-def sanitize(x):
-	hp = HTMLParser()
-	return hp.unescape( normalize('NFKD', u'%s' % x ).encode('ascii','ignore') )
-
-def tweetparse(tweet):
+def tweetparse(tweet, dedup):
 	if 'retweeted_status' in tweet:
-		u_handle = sanitize(tweet['user']['screen_name'])
-		u_src_handle =  sanitize(tweet['retweeted_status']['user']['screen_name'])
-		print '"%s" -- "%s"' % (u_handle, u_src_handle)
+		u_handle = sanitize(tweet['user']['screen_name']).lower()
+		u_src_handle =  sanitize(tweet['retweeted_status']['user']['screen_name']).lower()
+		link = '"%s" -> "%s"' % (u_src_handle, u_handle)
+		if link not in dedup:
+			dedup[link] = True
+			print link
 
 def main():
 	for f in sys.argv[1:] :
 		sys.stderr.write("# open(%s)\n" % f)
 		fd = open(f, 'r')
+		dedup = dict()
 		for js in fd.readlines() :
 			try:
 				tweet = json.loads(js)
-				tweetparse(tweet)
+				tweetparse(tweet, dedup)
 			except ValueError, KeyError:
 				pass
 		fd.close()
